85564214287057608831ef3233b78952e91d288b
susana mars <scmartins87@gmail.com>
2020-11-16 04:54:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,7 @@
 def display_board
- puts "A Tic Tac Toe Board"
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
-end# Def
-
-puts display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
